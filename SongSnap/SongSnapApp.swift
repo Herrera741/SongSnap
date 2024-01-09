ee7b86785cdbf4c7a05f03ab2344186bc366d767
Sergio <sherrera725@gmail.com>
2024-01-08 23:35:49 -0800

    Initial Commit

---
+++ SongSnap/SongSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongSnapApp.swift
+//  SongSnap
+//
+//  Created by Sergio Herrera on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SongSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
