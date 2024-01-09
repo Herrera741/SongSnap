2c62abd04d8f25a69dfd45a013b83ca9e79f0529
Sergio <sherrera725@gmail.com>
2024-01-09 01:18:37 -0800

    implement navigation back button from lyrics screen to search screen

--- SongSnap/Core/View/ContentView.swift
+++ SongSnap/Core/View/ContentView.swift
@@ -10,66 +10,71 @@ import SwiftUI
 struct ContentView: View {
     @State private var artist: String = ""
     @State private var song: String = ""
+    @State private var showLyrics: Bool = false
     
     var body: some View {
-        
-        ZStack {
-            Color.mint.ignoresSafeArea()
-            
-            VStack {
-                HStack(spacing: 20) {
-                    Image(systemName: "music.quarternote.3")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 45, height: 45)
-                    
-                    Text("SongSnap")
-                        .font(.title)
-                    .fontWeight(.bold)
-                    
-                    Image(systemName: "music.quarternote.3")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 45, height: 45)
-                } // end HStack
+        NavigationStack {
+            ZStack {
+                Color.mint.ignoresSafeArea()
                 
-                VStack(spacing: 15) {
-                    TextField("Enter name of artist", text: $artist)
-                        .padding()
-                        .background()
-                        .clipShape(
-                            RoundedRectangle(cornerRadius: 10)
-                        )
+                VStack {
+                    HStack(spacing: 20) {
+                        Image(systemName: "music.quarternote.3")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 45, height: 45)
+                        
+                        Text("SongSnap")
+                            .font(.title)
+                            .fontWeight(.bold)
+                        
+                        Image(systemName: "music.quarternote.3")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 45, height: 45)
+                    } // end HStack
                     
-                    TextField("Enter name of song", text: $song)
-                        .padding()
-                        .background()
-                        .clipShape(
-                            RoundedRectangle(cornerRadius: 10)
-                        )
-                    
-                    Button(action: {
-                        print("searching lyrics")
-                    }, label: {
-                        Text("Search Lyrics")
+                    VStack(spacing: 15) {
+                        TextField("Enter name of artist", text: $artist)
                             .padding()
-                            .foregroundStyle(.white)
-                            .frame(maxWidth: .infinity)
-                            .frame(height: 55)
-                            .background(.blue)
-                            .foregroundStyle(.white)
-                            .font(.headline)
+                            .background()
                             .clipShape(
                                 RoundedRectangle(cornerRadius: 10)
                             )
-                    })
-                    .padding(.top, 20)
-                    
+                        
+                        TextField("Enter name of song", text: $song)
+                            .padding()
+                            .background()
+                            .clipShape(
+                                RoundedRectangle(cornerRadius: 10)
+                            )
+                        
+                        Button(action: {
+                            print("searching lyrics")
+                            showLyrics = true
+                        }, label: {
+                            Text("Search Lyrics")
+                                .padding()
+                                .foregroundStyle(.white)
+                                .frame(maxWidth: .infinity)
+                                .frame(height: 55)
+                                .background(.blue)
+                                .foregroundStyle(.white)
+                                .font(.headline)
+                                .clipShape(
+                                    RoundedRectangle(cornerRadius: 10)
+                                )
+                        })
+                        .padding(.top, 20)
+                        .navigationDestination(isPresented: $showLyrics) {
+                            LyricsView()
+                        }
+                    } // end VStack
+                    .padding(.top, 30)
                 } // end VStack
-                .padding(.top, 30)
-            } // end VStack
-            .padding()
-        } // end ZStack
+                .padding()
+            } // end ZStack
+        } // end NavStack
     }
 }
 
