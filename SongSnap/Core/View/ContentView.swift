7b486eb6ab43c36f0f5ac4b0b700c49dd94b6ad7
Sergio <sherrera725@gmail.com>
2024-01-09 00:35:22 -0800

    add home screen ui components

--- SongSnap/Core/View/ContentView.swift
+++ SongSnap/Core/View/ContentView.swift
@@ -8,14 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var artist: String = ""
+    @State private var song: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        ZStack {
+            Color.mint.ignoresSafeArea()
+            
+            VStack {
+                HStack(spacing: 20) {
+                    Image(systemName: "music.quarternote.3")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 45, height: 45)
+                    
+                    Text("SongSnap")
+                        .font(.title)
+                    .fontWeight(.bold)
+                    
+                    Image(systemName: "music.quarternote.3")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 45, height: 45)
+                } // end HStack
+                
+                VStack(spacing: 15) {
+                    TextField("Enter name of artist", text: $artist)
+                        .padding()
+                        .background()
+                        .clipShape(
+                            RoundedRectangle(cornerRadius: 10)
+                        )
+                    
+                    TextField("Enter name of song", text: $song)
+                        .padding()
+                        .background()
+                        .clipShape(
+                            RoundedRectangle(cornerRadius: 10)
+                        )
+                    
+                    Button(action: {
+                        print("searching lyrics")
+                    }, label: {
+                        Text("Search Lyrics")
+                            .padding()
+                            .foregroundStyle(.white)
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 55)
+                            .background(.blue)
+                            .foregroundStyle(.white)
+                            .font(.headline)
+                            .clipShape(
+                                RoundedRectangle(cornerRadius: 10)
+                            )
+                    })
+                    .padding(.top, 20)
+                    
+                } // end VStack
+                .padding(.top, 30)
+            } // end VStack
+            .padding()
+        } // end ZStack
     }
 }
 
